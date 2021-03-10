cba49758abbb53021ea2bf48b506b359bea9bcc3
Arktiica <74079496+Arktiica@users.noreply.github.com>
2021-03-10 11:52:01 -0600

    Added 8.7 Code Practice: Q3

---
+++ 8_7CodePractice-arrayElementLengths.py
@@ -0,0 +1,7 @@
+w = ["Algorithm", "Logic", "Filter", "Software", "Network", "Parameters", "Analyze", "Algorithm", "Functionality", "Viruses"]
+s = []
+
+for i in w:
+    s.append(len(i))
+for i in range (len(w)):
+    print(str(s[i]) + ": " + str(w[i]))
