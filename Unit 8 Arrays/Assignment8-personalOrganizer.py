17020b97ec8510cb195cd72e3244f8a192b5cdd5
Arktiica <74079496+Arktiica@users.noreply.github.com>
2021-03-11 20:56:11 -0600

    Edhesive Assignment 8: Personal Organizer *.py

---
+++ Unit 8 Arrays/Assignment8-personalOrganizer.py
@@ -0,0 +1,74 @@
+# Check for and adjust month input if necessary
+def validateMonth(month):
+    if month >= 1 and month <= 12:
+        return month
+    else:
+        return 1
+
+# Check for and adjust day input if necessary
+def leapYear(year):         # Determines leap year
+    if (year % 400 == 0):
+        return True
+    elif (year % 100 == 0):
+        return False
+    elif (year % 4 == 0):
+        return True
+
+def numberOfDays(month, year):    # Returns the number of days: 28, 29, 30, 31
+    if (month == 1 or month == 3 or month == 5 or month == 7 or month == 8 or month == 10 or month == 12):
+        return 31
+    elif (month == 4 or month == 6 or month == 9 or month == 11):
+        return 30
+    elif (month == 2):
+        if (leapYear(year) == True):
+            return 29
+        else:
+            return 28
+
+def validateDay(month, day, year): 
+    # print(numberOfDays(month, year))
+    # print(type(numberOfDays(month, year)))
+    if day < 1 or day > numberOfDays(month, year):
+        return 1
+    else:
+        return day
+
+
+# This function is used to print all events to the user in the format
+# Event
+# Date: Month Day, Year
+def printEvents(eventName, eventMonth, eventDay, eventYear):
+	months = ['January','February','March','April','May','June','July','August', 'September','October','November','December']
+	for i in range(len(eventName)):
+		print(eventName[i])
+		print("Date: " + str(months[eventMonth[i]-1]) + " " + str(eventDay[i]) + ", " + str(eventYear[i]))
+
+# This function is used to prompt, adjust and
+# append values to the 4 parallel arrays
+def addEvent():
+    while (True):
+        newName = input("What is the event: ")
+        newMonth = int(input("What is the month (number): "))
+        newDay = int(input("What is the day: "))
+        newYear = int(input("What is the year: "))
+        print("")
+
+
+        eventName.append(newName)
+        eventMonth.append(validateMonth(newMonth))
+        eventDay.append(validateDay(newMonth, newDay, newYear))
+        eventYear.append(newYear)
+
+        proceed = input("Do you want to enter another event? NO to stop: ")
+        if (proceed.lower() == 'no'):
+            printEvents(eventName, eventMonth, eventDay, eventYear)
+            break
+
+
+#*********** MAIN **********
+eventName = []
+eventMonth = []
+eventDay = []
+eventYear = []
+
+addEvent()
