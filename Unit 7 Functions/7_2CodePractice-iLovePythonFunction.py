84168dca1e2e71280108838a4cebd916c1fb02f8
Arktiica <74079496+Arktiica@users.noreply.github.com>
2021-03-12 07:26:13 -0600

    Create 7_2CodePractice-iLovePythonFunction.py

---
+++ Unit 7 Functions/7_2CodePractice-iLovePythonFunction.py
@@ -0,0 +1,8 @@
+def ilovepython():
+    print('I love Python')
+    print('I love Python')
+    print('I love Python')
+
+ilovepython()
+# Disclaimer I don't actually like Python.
+# It's painful sometimes.
